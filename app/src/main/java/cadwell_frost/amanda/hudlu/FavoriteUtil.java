2805e1b68f29c3fa5db8bdba28e0c7aba36f19e6
Amanda Cadwell-Frost <amanda.cadwellfrost@hudl.com>
2016-04-28 23:20:40 -0400

    flesh out queries for hw 4.7

--- app/src/main/java/cadwell_frost/amanda/hudlu/FavoriteUtil.java
+++ app/src/main/java/cadwell_frost/amanda/hudlu/FavoriteUtil.java
@@ -1,9 +1,12 @@
 package cadwell_frost.amanda.hudlu;
 
+import android.content.Context;
+
 import cadwell_frost.amanda.hudlu.models.Favorite;
 import cadwell_frost.amanda.hudlu.models.MashableNewsItem;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
 import io.realm.RealmResults;
-import io.realm.internal.Context;
 
 /**
  * Created by amanda.cadwellfrost on 4/28/16.
@@ -12,21 +15,55 @@ public class FavoriteUtil {
 
     public static void addFavorite(Context context, MashableNewsItem newsItem)
     {
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder(context).build();
+        Realm realm = Realm.getInstance(realmConfig);
 
+        realm.beginTransaction();
+        Favorite favorite = realm.createObject(Favorite.class);
+        favorite.setTitle(newsItem.title);
+        favorite.setAuthor(newsItem.author);
+        favorite.setImage(newsItem.feature_image);
+        favorite.setLink(newsItem.link);
+        realm.commitTransaction();
     }
 
     public static void removeFavorite(Context context, MashableNewsItem newsItem)
     {
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder(context).build();
+        Realm realm = Realm.getInstance(realmConfig);
 
+        RealmResults<Favorite> results = realm.where(Favorite.class)
+                .equalTo("_link", newsItem.link)
+                .equalTo("_author", newsItem.author)
+                .equalTo("_image", newsItem.feature_image)
+                .equalTo("_title", newsItem.title)
+                .findAll();
+
+        realm.beginTransaction();
+        results.deleteAllFromRealm();
+        realm.commitTransaction();
     }
 
     public static boolean isFavorite(Context context, MashableNewsItem newsItem)
     {
-        return false;
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder(context).build();
+        Realm realm = Realm.getInstance(realmConfig);
+
+        RealmResults<Favorite> results = realm.where(Favorite.class)
+                .equalTo("_link", newsItem.link)
+                .equalTo("_author", newsItem.author)
+                .equalTo("_image", newsItem.feature_image)
+                .equalTo("_title", newsItem.title)
+                .findAll();
+
+        return !results.isEmpty();
     }
 
     public static RealmResults<Favorite> getAllFavorites(Context context)
     {
-        return null;//new RealmResults<Favorite>();
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder(context).build();
+        Realm realm = Realm.getInstance(realmConfig);
+
+        return realm.allObjects(Favorite.class);
     }
 }
--- app/src/main/java/cadwell_frost/amanda/hudlu/models/Favorite.java
+++ app/src/main/java/cadwell_frost/amanda/hudlu/models/Favorite.java
@@ -6,9 +6,49 @@ import io.realm.RealmObject;
  * Created by amanda.cadwellfrost on 4/28/16.
  */
 public class Favorite extends RealmObject {
-    private String title;
-    private String author;
-    private String image;
-    private String link;
+    private String _title;
+    private String _author;
+    private String _image;
+    private String _link;
+
+    public void setTitle(String title)
+    {
+        _title = title;
+    }
+
+    public String getTitle()
+    {
+        return _title;
+    }
+
+    public void setAuthor(String author)
+    {
+        _author = author;
+    }
+
+    public String getAuthor()
+    {
+        return _author;
+    }
+
+    public void setImage(String image)
+    {
+        _image = image;
+    }
+
+    public String getImage()
+    {
+        return _image;
+    }
+
+    public void setLink(String link)
+    {
+        _link = link;
+    }
+
+    public String link()
+    {
+        return _link;
+    }
 
 }
